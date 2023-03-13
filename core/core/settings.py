cbe38b0079082c4d5c98d77ab7ff88789359be61
FArooghOghba <faroogh.oghba@gmail.com>
2023-03-13 18:53:31 +0330

    build: create app config

--- core/core/settings.py
+++ core/core/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eb=+iy8!zci*oq8#l*m1lx#wd$im*z6sm)ov8xf77x-aimn$v6'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", default=False)
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'config.apps.ConfigConfig'
 ]
 
 MIDDLEWARE = [
